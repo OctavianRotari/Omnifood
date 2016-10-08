a11fd9189c33c6b34809882ed344b7eac0e4879e
Octavian Rotari <octavianrotari@gmail.com>
2016-10-08 11:48:19 +0200

    animations

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -19,7 +19,6 @@ $(document).ready(function(){
 	});
 
 	//navigation
-	//
 	$(function() {
 		$('a[href*="#"]:not([href="#"])').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -34,4 +33,29 @@ $(document).ready(function(){
 			}
 		});
 	});
-})
+
+	//animations on scroll
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-2').waypoint(function(direction){
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-3').waypoint(function(direction){
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-4').waypoint(function(direction){
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+});
